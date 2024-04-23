0934db033b2edf46fff1319bd12a3a41152f941d
Josias Neto <josiasnieto@hotmail.com>
2024-04-23 16:09:01 +0200

    Game Loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,6 +8,8 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable{
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
 	
 	//---------------------------------------------------- Window Size Settings -/
 	
@@ -18,7 +20,15 @@ public class Game extends Canvas implements Runnable{
 	//---------------------------------------------------- Game Constructor -/
 	
 	public Game() {
-		this.setPreferredSize(new Dimension(width * scale, height * scale));
+		
+		setPreferredSize(new Dimension(width * scale, height * scale));
+		startFrame();
+	}
+	
+	//---------------------------------------------------- Frame Render Function -/
+	
+	public void startFrame() {
+		
 		frame = new JFrame("My Game");
 		frame.add(this);
 		frame.setResizable(false);
@@ -28,13 +38,64 @@ public class Game extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	
-	public static void main(String args[]) {
-		Game game = new Game();
+	//---------------------------------------------------- Game Start -/
+	
+	public synchronized void start() {
+		
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
 	}
+	
+	//---------------------------------------------------- Game Stop -/
+	
+	public synchronized void stop() {
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	//---------------------------------------------------- Game Loop -/
 	
 	public void run() {
 		
+		long lastTime = System.nanoTime();
+		double FPS = 60;
+		double ns = 1000000000 / FPS;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
 		
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns; 
+			lastTime = now;
+			
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
 	}
 	
 }
